27ca3339fa6b2f83a26a148fef8e328a24ee911b
fde-capu <52455203+fde-capu@users.noreply.github.com>
2024-06-06 15:01:38 +0000

    1st class finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div><h2>Rick Sanchez</h2>
+      <div class="card"><h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
