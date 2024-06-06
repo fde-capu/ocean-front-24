5cbf1d15481c47e65225f0a44a1d9f5bdfba7969
fde-capu <52455203+fde-capu@users.noreply.github.com>
2024-06-06 14:29:33 +0000

    estrutura html criada

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      App
+      <div><h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
     </>
   )
 }
