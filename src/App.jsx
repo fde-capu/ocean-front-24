17868d5a637bfd7a712713e74925e884e1dcfb36
fde-capu <52455203+fde-capu@users.noreply.github.com>
2024-06-07 12:37:39 +0000

    desafio 1 ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <div class="card"><h2>Rick Sanchez</h2>
+        <tag>fool</tag>
+        <tag>scientis</tag>
+        <tag>burps a lot</tag>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
