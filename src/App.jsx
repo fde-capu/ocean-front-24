819d7e5595dd1b17a098ac6d847be59620079746
fde-capu <52455203+fde-capu@users.noreply.github.com>
2024-06-07 14:49:29 +0000

    Mitigados warnings.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import './App.css'
 import Card from './Components/Card/Card'
 
 const item1 = {
-  key: 1,
   name: "Rick Sanches",
   image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
 }
 
 const item2 = {
-  key: 2,
   name: "Morty Smith",
   image: "https://vignette1.wikia.nocookie.net/rickandmorty/images/3/36/Morty_Interested_S1E11.JPG/revision/latest?cb=20150908101833"
 }
 
 const item3 = {
-  key: 3,
   name: "Mr. Poopybutthole",
   image: "https://i.imgur.com/2rG8iKu.png"
 }
@@ -28,7 +25,7 @@ function App() {
         {
           items.map(function (e) {
             return (
-              <Card item={e} />
+              <Card item={e} key={e.name} />
             )
           })
         }
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,9 +3,9 @@ export default function Card(props) {
     return (
         <div className="card">
         <h2>{item.name}</h2>
-        <div class="tag">fool</div>
-        <div class="tag">scientist</div>
-        <div class="tag">burps a lot</div>
+        <div className="tag">fool</div>
+        <div className="tag">scientist</div>
+        <div className="tag">burps a lot</div>
         <img src={item.image} />
         </div>
     )
