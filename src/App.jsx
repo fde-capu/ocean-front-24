df1924686b3e1e3ded303f009f8c4f10f1ffedf4
fde-capu <52455203+fde-capu@users.noreply.github.com>
2024-06-13 14:17:39 +0000

    vim

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './Components/Card/Card'
 
-const item1 = {
-  name: "Rick Sanches",
-  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-}
+//const item1 =
+//
+//{
+//  name: "Rick Sanches",
+//  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+//}
+//
+//const item2 = {
+//  name: "Morty Smith",
+//  image: "https://vignette1.wikia.nocookie.net/rickandmorty/images/3/36/Morty_Interested_S1E11.JPG/revision/latest?cb=20150908101833"
+//}
+//
+//const item3 = {
+//  name: "Mr. Poopybutthole",
+//  image: "https://i.imgur.com/2rG8iKu.png"
+//}
 
-const item2 = {
-  name: "Morty Smith",
-  image: "https://vignette1.wikia.nocookie.net/rickandmorty/images/3/36/Morty_Interested_S1E11.JPG/revision/latest?cb=20150908101833"
-}
-
-const item3 = {
-  name: "Mr. Poopybutthole",
-  image: "https://i.imgur.com/2rG8iKu.png"
-}
-
-const items = [item1, item2, item3];
 
 function App() {
+
+  const [items, setItens] = useState([])
+
+  async function carregarDados() {
+    const apiUrl = "https://rickandmortyapi.com/api/character"
+    const response = await fetch(apiUrl)
+    const body = await response.json()
+    const results = body.results
+    console.log('results', results)
+    setItens(results);
+  }
+
+  useEffect(function() {
+    carregarDados()
+  }, [])
+
   return (
     <>
       <div className="cards">
@@ -30,6 +48,7 @@ function App() {
       </div>
     </>
   )
+
 }
 
 export default App
